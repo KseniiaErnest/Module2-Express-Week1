8209bf62c1d7709176e14820912d5410aa965451
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-05-06 17:50:01 -0400

    Next the each block helper

--- app.js
+++ app.js
@@ -11,6 +11,33 @@ app.set('views', __dirname + 'views');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
+// 1. send views/index.hbs for displaying in the browser
+// 2. Add additional parameter that will contain a JS object with info we can use in the view
+// app.get('/', (req, res, next) => {
+//   let data = {
+//     name: 'Kseniia',
+//     bootcamp: 'Ironhack Web Dev'
+//   };
+
+//   res.render('index', data);
+// })
+
+// 3. The if block helper - if its argument returns false, undefined, null, "", 0, or [], Handlebars will not render the block 
+app.get('/', (req, res, next) => {
+  let data = {
+    name: 'Kseniia',
+    lastName: 'Ernest'
+  };
+
+  res.render('index', data);
+})
+
+
+
+app.get('/about', (req, res, next) => res.render('about'));
+
+
+
 
 
 
